e7dd29352f8a886bcd2fb4af379d0bae73a85dfd
aras23 <104873579+aras23@users.noreply.github.com>
2022-05-25 00:00:43 -0700

    Initial commit

---
+++ Frontend/src/app/components/crud-rol/crud-rol.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-rol',
+  templateUrl: './crud-rol.component.html'
+})
+export class CrudRolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
